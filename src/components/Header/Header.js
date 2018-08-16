f705105842d2ee1dd2ad1a0d3610d80743a94315
Tyler McNamara <mcnamara14@gmail.com>
2018-08-16 09:40:48 -0600

    Add filters section with refresh and checkboxes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './Header.css';
+import refreshIcon from './images/refresh.svg';
 
 class Header extends Component {
   render() {
     return (
       <section className="Header" >
           <h1>SpaceX Launches</h1>
+          <section className="headerFilters">
+            <div className="refreshIcon"><img src={refreshIcon} /></div>
+            <div className="checkboxes">
+              <label class="container landSuccess">LAND SUCCESS
+                <input type="checkbox" />
+                <span class="checkmark"></span>
+              </label>
+              <label class="container reused">REUSED
+                <input type="checkbox" />
+                <span class="checkmark"></span>
+              </label>
+              <label class="container withReddit">WITH REDDIT
+                <input type="checkbox" />
+                <span class="checkmark"></span>
+              </label>
+            </div>
+          </section>
       </section>
     );
   }
