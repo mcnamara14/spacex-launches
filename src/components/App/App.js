1c8982f3c08131ee12f31921a36983be0e16569d
Tyler McNamara <mcnamara14@gmail.com>
2018-08-15 23:36:03 -0600

    Add normalize css and background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import spaceBgImg from './images/background.jpg';
 
 class App extends Component {
   render() {
+    const bgImg = {backgroundImage: "url(" + spaceBgImg + ")"};
+
     return (
-      <div className="App">
-        <h1>Working?</h1>
-      </div>
+      <section className="App" style={bgImg}>
+        <main>
+          <h1>SpaceX Launches</h1>
+        </main>
+      </section>
     );
   }
 }
