50f5f87939cac74e40d5c91176e1012805791113
Tyler McNamara <mcnamara14@gmail.com>
2018-08-16 11:02:45 -0600

    Add LaunchCard cells

--- src/components/LaunchCard/LaunchCard.js
+++ src/components/LaunchCard/LaunchCard.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './LaunchCard.css';
+import placeholderImg from './images/placeholder.png';
 
 class LaunchCard extends Component {
   render() {
     return (
       <section className="LaunchCard" >
-        
+          <div className="badge"><img src={placeholderImg} /></div>
+          <div className="name">Name</div>
+          <div className="type">Type</div>
+          <div className="date">Date</div>
+          <div className="details">Details</div>
+          <div className="id">ID</div>
+          <div className="article">Article</div>
       </section>
     );
   }
