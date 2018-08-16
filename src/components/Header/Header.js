d5abca0748da66eeeaac3809c2ad015c1037007d
Tyler McNamara <mcnamara14@gmail.com>
2018-08-16 09:57:37 -0600

    Add HeaderLabels component with labels

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Header.css';
+import HeaderFilters from '../HeaderFilters/HeaderFilters';
+import HeaderLabels from '../HeaderLabels/HeaderLabels';
 import refreshIcon from './images/refresh.svg';
 
 class Header extends Component {
@@ -7,23 +9,8 @@ class Header extends Component {
     return (
       <section className="Header" >
           <h1>SpaceX Launches</h1>
-          <section className="headerFilters">
-            <div className="refreshIcon"><img src={refreshIcon} /></div>
-            <div className="checkboxes">
-              <label class="container landSuccess">LAND SUCCESS
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-              </label>
-              <label class="container reused">REUSED
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-              </label>
-              <label class="container withReddit">WITH REDDIT
-                <input type="checkbox" />
-                <span class="checkmark"></span>
-              </label>
-            </div>
-          </section>
+          <HeaderFilters />
+          <HeaderLabels />
       </section>
     );
   }
