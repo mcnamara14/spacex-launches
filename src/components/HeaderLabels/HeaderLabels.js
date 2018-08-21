71e713f97b32c6bb31da55b4027972c94d635eb9
Tyler McNamara <mcnamara14@gmail.com>
2018-08-21 13:28:30 -0600

    Add styling for sort indicator below and above

--- src/components/HeaderLabels/HeaderLabels.js
+++ src/components/HeaderLabels/HeaderLabels.js
@@ -4,17 +4,35 @@ import './HeaderLabels.css';
 import { toggleDateSort } from '../../actions/toggleDateSort';
 
 class HeaderLabels extends Component {
-  toggleSort = () => {
+  constructor() {
+    super()
+
+    this.state = {
+      sort: false
+    }
+  }
+
+  handleClick = () => {
     this.props.toggleDateSort();
+
+    if (!this.state.sort) {
+      this.setState({sort: true})
+    } else {
+      this.setState({sort: false})
+    }
   }
 
   render() {
+    let dateClass;
+
+    this.state.sort ? dateClass = 'date asc' : dateClass = 'date desc';
+
     return (
       <section className="HeaderLabels" >
           <h2 className="badge">Badge</h2>
           <h2 className="name">Rocket Name</h2>
           <h2 className="type">Rocket Type</h2>
-          <h2 className="date" onClick={() => this.toggleSort()}>Launch Date</h2>
+          <h2 className={dateClass} onClick={() => this.handleClick()}>Launch Date</h2>
           <h2 className="details">Details</h2>
           <h2 className="id">ID</h2>
           <h2 className="article">Article</h2>
